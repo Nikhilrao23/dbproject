3d8c8bd14575a5bfcc1bb032eebe8386de86453d
Nikhil Rao Balaji <raonikhil23@gmail.com>
2016-05-09 23:49:45 -0400

    Mongodb Connection and Insertion

--- add.php
+++ add.php
@@ -1,5 +1,10 @@
 <?php
 include_once('connection.php');
+$uri="mongodb://raonikhil23:Keepcalm23@ds017862.mlab.com:17862/inventory_db";
+$conn=new Mongoclient($uri);
+$db=$conn->inventory_db;
+$productscollection=$db->products;
+
 echo"Enter the product id".PHP_EOL;
 $handle=fopen("php://stdin","r");
 $line=fgets($handle);
@@ -13,7 +18,11 @@ $Pprice = trim($line3);
 echo"Enter the Product Quantity".PHP_EOL;
 $line4= fgets($handle);
 $Pquantity= trim($line4);
-
+$productinfo=array('ProductID'=> $PID,
+		'Productname'=>$Pname,
+	 	'Price'=>$Pprice,
+		'Quantity'=>$Pquantity);
+$productscollection->insert($productinfo);
 mysql_query("Insert into products values('', '$Pname','$Pprice','$Pquantity')");
 echo "Products added".PHP_EOL;
 include 'mandet.php';
